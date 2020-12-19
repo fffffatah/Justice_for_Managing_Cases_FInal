f6aea9c2772a1f951fb8268fc3a13b25c67137fa
Muhammad Fatah <ab.fatahmn@gmail.com>
2020-12-19 19:54:58 +0600

    Add sendgrid api and test codes

--- controllers/lawyer_controller.php
+++ controllers/lawyer_controller.php
@@ -1,3 +1,5 @@
 <?php
     require_once '../models/db_conn.php';
+    require_once '../models/mail_sender.php';
+    sendOtp("Fatah","nepsevukna@yevme.com","abcd")
 ?>
---
+++ models/mail_sender.php
@@ -0,0 +1,24 @@
+<?php
+    require_once '../dependencies/sendgrid-php/sendgrid-php.php';
+?>
+<?php
+    function sendOtp($username, $address, $otp){
+        $email=new \SendGrid\Mail\Mail();
+        $email->setFrom("no-reply@justicecms.com", "Justice - OTP");
+        $email->setSubject("Your Justice OTP");
+        $email->addTo($address, $username);
+        $email->addContent("text/plain", "Your OTP for Password Reset/Account Creation: ");
+        $email->addContent("text/html", "<strong>".$otp."</strong>");
+
+        $sendgrid=new \SendGrid(getenv('SENDGRID_API_KEY'));
+        try{
+            $response=$sendgrid->send($email);
+            print $response->statusCode()."\n";
+            print_r($response->headers());
+            print $response->body()."\n";
+        }
+        catch (Exception $e){
+            echo 'Caught exception: '. $e->getMessage() ."\n";
+        }   
+    }
+?>
