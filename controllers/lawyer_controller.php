b3f1830a59ccb202fe9a94726366bc58fbd98c13
Muhammad Fatah <ab.fatahmn@gmail.com>
2020-12-19 18:55:05 +0600

    Revert database test code

--- controllers/lawyer_controller.php
+++ controllers/lawyer_controller.php
@@ -1,7 +1,3 @@
 <?php
     require_once '../models/db_conn.php';
-    $results=doQuery("SELECT * FROM sample");
-    foreach($results as $result){
-        echo "<b>".$result["name"]."</b>";
-    }
 ?>
